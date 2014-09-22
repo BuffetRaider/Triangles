d6fabc24d6b6fb581e3eba1cc9cdab13aeb13aba
JackYoung <pjyxv4@mail.mst.edu>
2014-09-21 21:10:59 -0500

    d

---
+++ Triangles/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             for (int i = 0; i < 10; i++)           //  *
+             {                                      //  **
+                     for(int n = 0; n < i; n++)     //  ***
+                     {                              //  ****
+                         Console.Write('*');        //  *****
+                     }                              //  ******
+                 Console.WriteLine();               //  *******
+             }                                      //  ********
+
+            Console.WriteLine();                    // blank line before next pattern
+
+             for (int i = 0; i < 10; i++)           //  ********
+             {                                      //  *******
+                    for (int n = 0; n < 9 - i; n++) //  ******
+                     {                              //  *****
+                         Console.Write('*');        //  ****
+                     }                              //  ***
+                 Console.WriteLine();               //  **
+             }                                      //  *
+
+            Console.WriteLine();                    // blank line before next pattern
+
+            for (int i = 0; i < 10; i++)
+            {
+                    for (int n = 0; n < 9 - i; n++) //       *
+                     {                              //      **
+                         Console.Write(' ');        //     ***
+                                                    //    ****
+                     }                              //   *****
+                    for (int x = 0; x <= i; x ++)   //  ******
+                    {                               // *******
+                        Console.Write('*');
+                    }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();                    // blank line before next pattern
+
+            for (int i = 0; i < 10; i++)            // *******
+            {                                       //  ******
+                for (int n = 0; n < i; n++)         //   *****
+                {                                   //    ****
+                    Console.Write(' ');             //     *** 
+                                                    //      **
+                }                                   //       *
+                for (int x = 0; x <= 9 - i; x++)    
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+                Console.Read();              
+            }
+
+        } 
+    }
+
